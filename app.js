b96b86c076bd1be2e16fdc5a9d0830b98dcfeb0a
Ankit Shaji <ankitshaji@gmail.com>
2022-07-21 04:33:11 +1200

    adding gameOver conditions

--- app.js
+++ app.js
@@ -7,20 +7,28 @@ const p2Span = document.querySelector("#p2Span");
 //starting score variables
 let p1Score = 0;
 let p2Score = 0;
+//game ending variables
+let isGameOver = false;
 let winningScore = 5;
 
 //eventListner method on buttonElementObject
 p1Button.addEventListener("click", function () {
-  if (p1Score !== winningScore) {
+  if (!isGameOver) { //if(true)
     p1Score++;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
     p1Span.textContent = p1Score;
   }
 });
 
 //eventListner method on buttonElementObject
 p2Button.addEventListener("click", function () {
-  if (p2Score !== winningScore) {
+  if (!isGameOver) { //if(true)
     p2Score++;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
     p2Span.textContent = p2Score;
   }
 });
